ffe31c4fb7e7648ca318fafba64274a5e5b5a7b4
Patrick Langer <patrick.langer97@gmx.de>
2025-04-26 16:39:46 -0700

    Added m4 dataset loader. Added test folder.

---
+++ constants.py
@@ -0,0 +1,9 @@
+# constants.py
+
+import os
+
+# Path to this file's directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Path to raw data directory
+RAW_DATA = os.path.join(BASE_DIR, "raw_data")
