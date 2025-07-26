aec0c6222f9b398c9a03bf97fe89fc9cb603964d
Patrick Langer <patrick.langer97@gmx.de>
2025-07-26 14:48:23 +0200

    Debug

--- src/time_series_datasets/sleep/SleepEDFCoTQADataset.py
+++ src/time_series_datasets/sleep/SleepEDFCoTQADataset.py
@@ -47,6 +47,8 @@ class SleepEDFCoTQADataset(QADataset):
         std = max(std, min_std)
         series_norm = (series - mean) / std
         text_prompt = f"The following is the EEG time series, it has mean {mean:.4f} and std {std:.4f}:"
+        print(series_norm.tolist())
+        exit(0)
         return [TextTimeSeriesPrompt(text_prompt, series_norm.tolist())]
 
     @staticmethod
