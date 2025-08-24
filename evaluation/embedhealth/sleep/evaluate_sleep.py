remove normalization; fix replacement of mean and std

evaluation/embedhealth/sleep/evaluate_sleep.py
@@ -129,24 +129,19 @@ def main():
                 for ts_text, ts_data in zip(
                     row["time_series_text"], row["time_series"]
                 ):
-                    # fix no normalization
-                    mean = np.mean(ts_data)
-                    std = np.std(ts_data)
-                    normalized_ts_data = (ts_data - mean) / std
-
                     # fix 0.000 mean and std
                     ts_text = re.sub(
                         r"mean -?\d+\.?\d+",
-                        f"mean {np.mean(normalized_ts_data):.3f}",
+                        f"mean {round(np.mean(ts_data), 4):.4f}",
                         ts_text,
                     )
                     ts_text = re.sub(
                         r"std -?\d+\.?\d+",
-                        f"std {np.mean(normalized_ts_data):.3f}",
+                        f"std {round(np.std(ts_data), 4):.4f}",
                         ts_text,
                     )
 
-                    ts_prompts.append(TextTimeSeriesPrompt(ts_text, normalized_ts_data))
+                    ts_prompts.append(TextTimeSeriesPrompt(ts_text, ts_data))
 
                 # Create full prompt
                 prompt = FullPrompt(pre_prompt, ts_prompts, post_prompt)