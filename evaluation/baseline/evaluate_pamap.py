c323dc346bcafd24e737559d99389a89df6e4ea6
Max Rosenblattl <max.rosenblattl@tum.de>
2025-08-28 00:35:20 +0200

    Run HAR Acc with 4o

--- evaluation/baseline/evaluate_pamap.py
+++ evaluation/baseline/evaluate_pamap.py
@@ -62,7 +62,6 @@ def main():
         dataset_classes=dataset_classes,
         evaluation_functions=evaluation_functions,
         max_samples=None,  # Limit for faster testing, set to None for full evaluation,
-        use_plot=args.plot,
         max_new_tokens=400,
     )
     print("\n" + "="*80)
