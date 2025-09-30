b5cb7ceec4872c3ed151feea249069afc0ecfb9c
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-09-30 11:22:48 -0700

    Update REUSE

--- src/prompt/__init__.py
+++ src/prompt/__init__.py
@@ -6,6 +6,7 @@
 # SPDX-License-Identifier: MIT
 #
 
+# from .prompt import Prompt
 # from .full_prompt import FullPrompt
 # from .text_prompt import TextPrompt
 # from .text_time_series_prompt import TextTimeSeriesPrompt
